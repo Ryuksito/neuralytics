bug en la capa de batch normalization, no se mantienen las dimensiones en gamma

neuralytics/models/nn/layers/abstract_layer.py
@@ -2,15 +2,14 @@ import numpy as np
 
 class AbstractLayer:
 
-    def __init__(self, input_shape: tuple, neurons: int, activation: callable = None):
+    def __init__(self, input_shape: tuple, neurons: int):
         self.input_shape = input_shape
         self.neurons = neurons
-        self.activation = activation
 
         self.w = np.random.randn(np.prod(input_shape), neurons) / np.sqrt(np.prod(input_shape)/2)
         self.b = np.zeros((1,neurons))
-        self.z: np.ndarray
-        self.a: np.ndarray
+        self.z: np.ndarray = None
+        self.a:np.ndarray = None
 
     def __call__(self, x: np.ndarray):
         raise NotImplementedError
@@ -18,6 +17,6 @@ class AbstractLayer:
     def get_weights(self):
         return self.w, self.b
 
-    def set_weights(self, w: np.ndarray, b: np.ndarray):
-        self.w = w
-        self.b = b
+    def set_weights(self, parameters:tuple):
+        self.w = parameters[0]
+        self.b = parameters[1]

neuralytics/models/nn/layers/batch_norm.py
@@ -1,36 +1,71 @@
 import numpy as np
+import pdb
 
 class BatchNorm: 
 
     nType = 'layer'
+    layer_name = 'batchnorm'
     
-    def __init__(self, input_shape:tuple, neurons:int, e:np.float32, name:str='BatchNorm'):
-        self.y = np.random.randn(np.prod(input_shape), neurons) / np.sqrt(np.prod(input_shape)/2)   # gamma
-        self.B = np.zeros((1,neurons))   # beta
+    def __init__(self, neurons:int, e:np.float32, name:str='BatchNorm'):
+        self.y = np.ones((1, neurons))   # gamma
+        self.B = np.zeros((neurons,1))   # beta
         self.e = e   # epsilon
         self.name = name
+        self.a: np.ndarray = None
+        self.xi_hat = None
+        self.std = None
+        self.mean = None
     
     def __str__(self):
-        return f"\nDense Layer:\nGamma:\n{self.y}\nBeta:\n{self.B}"
+        return f"\BatchNorm Layer: {self.name}\nGamma:\n{self.y.shape}\nBeta:\n{self.B.shape}"
 
     def __call__(self, x: np.ndarray):
 
-        xi = (x - np.mean(x))/np.std(x) + self.e
-        yi = xi @ self.y + self.B
+        self.std = np.std(x)
+        self.mean = np.mean(x)
+        self.xi_hat = (x - self.mean)/self.std + self.e
+        self.a = self.xi_hat @ self.y.T + self.B
 
-        return yi
+        return self.a
+    
+    def get_weights(self):
+        return self.y, self.B
+
+    def set_weights(self, parameters:tuple):
+        self.y = parameters[0]
+        self.B = parameters[1]
+    
+    def backward(self, lr:float, deltaL_1: np.ndarray, aL_1:np.ndarray, daL_1_dzL_1:np.ndarray, l:int):
+
+        # calculo del error
+        pdb.set_trace()
+        ey =  deltaL_1.T @ self.xi_hat
+        eB = deltaL_1
+        eB = np.sum(eB, axis=0, keepdims=True)
+
+        y = self.y - ey.T * lr
+        B = self.B - eB * lr
+
+        if l != 0: 
+            d_std = (aL_1 - self.mean) * ((1/2)*((self.std + self.e)**(-3/2)))
+            d_mean = (-1/self.std + self.e) + d_std * -2 * (aL_1 - self.mean)
+            deltaL_1 = (deltaL_1 @ self.y.T) * (1/self.std + self.e) + d_std * (2*(aL_1-self.mean)) + d_mean
+        else: 
+            deltaL_1 = None
+
+        return (deltaL_1, y, B)
     
 
-def fn(funct:callable):
-    if funct is callable:
-        print('is callable', funct(np.array([[1,2,3]])))
-    else:
-        print('is not callable', funct(np.array([[1,2,3]])))
+# def fn(funct:callable):
+#     if funct is callable:
+#         print('is callable', funct(np.array([[1,2,3]])))
+#     else:
+#         print('is not callable', funct(np.array([[1,2,3]])))
 
 
 
     
 
-fn(BatchNorm)
-print('------------------------------------------------')
-fn(BatchNorm())
+# fn(BatchNorm)
+# print('------------------------------------------------')
+# fn(BatchNorm())

neuralytics/models/nn/layers/dense.py
@@ -5,9 +5,10 @@ from .abstract_layer import AbstractLayer
 class Dense(AbstractLayer):
 
     nType = 'layer'
+    layer_name = 'dense'
 
-    def __init__(self, input_shape:tuple, neurons:int, activation:callable=None, name:str='Relu'):
-        super().__init__(input_shape, neurons, activation)
+    def __init__(self, input_shape:tuple, neurons:int, name:str='Relu'):
+        super().__init__(input_shape, neurons)
         self.name = name
         
     
@@ -21,16 +22,11 @@ class Dense(AbstractLayer):
 
         # Calcular la entrada a la capa
         self.z = flattened_input @ self.w + self.b
-
-        # Aplicar la función de activación
-        if(self.activation != None): 
-            self.a = self.activation(self.z)
-            return self.a
         
         self.a = self.z
         return self.a
     
-    def backward(self, lr:float, deltaL_1:np.ndarray, aL_1:np.ndarray, wL:np.ndarray, daL_1_dzL_1:np.ndarray, i:int) -> tuple: # (deltaL_1,w,b)
+    def backward(self, lr:float, deltaL_1:np.ndarray, aL_1:np.ndarray, daL_1_dzL_1:np.ndarray, i:int) -> tuple: # (deltaL_1,w,b)
         # calculo del error
         ew =  deltaL_1.T @ aL_1
         eb = deltaL_1
@@ -46,5 +42,6 @@ class Dense(AbstractLayer):
             deltaL_1 = None
         
         return  (deltaL_1, w, b)
+    
     
     

neuralytics/models/nn/secuencial.py
@@ -44,21 +44,30 @@ class Secuencial:
     def _backpropagation(self, lr, x, y):
         #backpropagation
         temp_wb = []
-        deltaL_1 = self.loss_function.df(y,self.layers[-1].a) * (self.layers[-1].activation.df(self.layers[-1].z) if hasattr(self.layers[-1], 'activation') and self.layers[-1].activation != None else np.ones(self.layers[-1].a.shape))
+
+        if self.layers[-1].nType == 'act':
+            deltaL_1 = self.loss_function.df(y, self.layers[-1].a) * self.layers[-1].df(self.layers[-2].a)
+        else:
+            deltaL_1 = self.loss_function.df(y, self.layers[-1].a) * np.ones(self.layers[-1].a.shape)
+        
+        # pdb.set_trace()
         
         for i,l in enumerate(reversed(self.layers)):
 
             i = len(self.layers)- 1 - i
+            print(f'Layer L{i-2}\n{l}')
+            # pdb.set_trace()
 
-            if not isinstance(l, (AbstractLayer, Dense)): 
-                temp_wb.append((None,None))
-                continue
-
-            aL_1 = (self.layers[i-1].a if i != 0 else x) # aL-i
-            wL = l.w.T
-            daL_1_dzL_1 = (self.layers[i-1].activation.df(self.layers[i-1].z) if hasattr(self.layers[i-1], 'activation') and self.layers[i-1].activation != None else np.ones(self.layers[i-1].a.shape))
-
-            deltaL_1, w, b = l.backward(lr, deltaL_1, aL_1, wL, daL_1_dzL_1, i) # learning rate, delta, 
+            aL_1 = (self.layers[i-1].a if i != 0 else x)
+            # pdb.set_trace()
+            if self.layers[-1].nType == 'act':
+                daL_1_dzL_1 = self.layers[i-1].activation.df(self.layers[i-2].z)
+            else:
+                daL_1_dzL_1 = np.ones(self.layers[i-1].a.shape)
+            pdb.set_trace()
+          
+            deltaL_1, w, b = l.backward(lr, deltaL_1, aL_1, daL_1_dzL_1, i) # learning rate, delta, 
+            pdb.set_trace()
 
 
             temp_wb.append((w,b))
@@ -67,10 +76,7 @@ class Secuencial:
                 
     def _update(self, wb:list):
         for i,l in enumerate(reversed(self.layers)):
-          if not isinstance(l, (AbstractLayer, Dense)): 
-              continue
-          l.set_weights(wb[i][0],wb[i][1])
-        pass           
+          l.set_weights(wb[i])        
 
     def add(self, layer):
         self.layers.append(layer)

tests/test_nn.py
@@ -11,25 +11,28 @@ class TestSimpleLinearRegression(unittest.TestCase):
     def test_nn_cf(self):
         import numpy as np
         from neuralytics.models.nn import Secuencial
-        from neuralytics.models.nn.layers import Dense
-        from neuralytics.models.nn import ReLU, MSE
+        from neuralytics.models.nn.layers import Dense, ReLu, BatchNorm
+        from neuralytics.models.nn import MSE
 
 
         
         #layers
 
-        dense1 = Dense(activation=ReLU, input_shape=(1), neurons=3, name='dense1')
+        dense1 = Dense(input_shape=(1), neurons=3, name='dense1')
 
 
-        dense2 = Dense(activation=ReLU, input_shape=(1,3), neurons=2, name='dense2')
+        dense2 = Dense(input_shape=(1,3), neurons=2, name='dense2')
+        batchNorm2 = BatchNorm(neurons=2, e=1e-8, name='batchNorm2')
 
 
         dense3 = Dense(input_shape=(1,2), neurons=1, name='dense3')
 
+        print(f'Initial state: \n{dense1}\n{dense2}\n{batchNorm2}\n{dense3}')
+
         
 
         #data
-        data_len = 9
+        data_len = 10
         data_step = 5
         x = np.array([((i-20)) for i in range(0,data_len*data_step,data_step)]).reshape(data_len,1)
         y = np.array([((i*9) / 5) + 32 for i in x]).reshape(data_len,1)
@@ -47,10 +50,17 @@ class TestSimpleLinearRegression(unittest.TestCase):
 
 
 
-        model = Secuencial(layers=[dense1, dense2, dense3])
+        model = Secuencial(layers=[
+            dense1, 
+            dense2, 
+            batchNorm2,
+            ReLu(),
+            dense3
+        ])
 
         model.compile(loss_function=MSE)
-        model.fit(x=x, y=y,epochs=1000,lr=0.01, batch_size=10)
+        model.fit(x=x, y=y,epochs=1,lr=0.01)
+        print(f'final state: \n{dense1}\n{dense2}\n{batchNorm2}\n{dense3}')
 
 
         print(x)